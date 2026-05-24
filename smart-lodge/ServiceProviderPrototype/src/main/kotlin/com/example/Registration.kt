cad8e451904976c1445f0ea386c37c8ba1061376
Hannes Morgenthaler <hannes@morgethaler.ch>
2026-05-24 15:36:32 +0200

    remove unused smart-home code.

--- smart-lodge/ServiceProviderPrototype/src/main/kotlin/com/example/Registration.kt
+++
@@ -1,8 +0,0 @@
-package com.example
-
-data class Registration(
-    val buildingID: String,
-    val xCoordinate: Int,   // Nullpunkt: Smarthome vorne links Blickrichtung Whiteboard
-    val yCoordinate: Int,
-    val owner: String,
-)
